7f069c005ee5495e9754036e4a59f30ee5ddcf52
Randi Julio Fajri <randijulio13@gmail.com>
2025-05-26 08:06:09 +0700

    fix scroll indicator

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,7 @@
 import Lottie from "lottie-react"
 import SectionWrapper from "./components/section-wrapper"
 import LottieScroll from "./assets/lottie-scroll.json"
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import Snowfall from 'react-snowfall'
 import HeroSection from "./components/HeroSection"
 import QuotesSection from "./components/QuotesSection"
@@ -10,17 +10,27 @@ import ExperienceSection from "./components/ExperienceSection"
 import ScrollIndicator from "./components/ScrollIndicator"
 import FooterSection from "./components/FooterSection"
 import SplashScreen from "./components/SplashScreen"
+import { useMotionValueEvent, useScroll } from "motion/react"
 
 const Home = () => {
     const mainRef = useRef<HTMLDivElement>(null);
 
+    const [scrollWidth, setScrollWidth] = useState(0);
+
+    const scroll = useScroll({
+        container: mainRef,
+    })
+
+    useMotionValueEvent(scroll.scrollYProgress, "change", (latest) => {
+        setScrollWidth(latest * 100)
+    })
 
 
     return (
         <>
             <SplashScreen />
             <div ref={mainRef} className="scroll-smooth snap-y snap-mandatory h-screen overflow-y-auto bg-primary text-foreground no-scrollbar">
-                <ScrollIndicator mainRef={mainRef} />
+                <ScrollIndicator progress={scrollWidth} />
                 <Snowfall
                     color="white"
                     snowflakeCount={10}
--- src/components/ScrollIndicator.tsx
+++ src/components/ScrollIndicator.tsx
@@ -1,26 +1,14 @@
-import { useMotionValueEvent, useScroll } from "motion/react";
-import { useState, type RefObject } from "react";
 
 interface ScrollIndicatorProps {
-    mainRef: RefObject<HTMLDivElement | null>
+    progress: number
 }
 
-const ScrollIndicator = ({ mainRef }: ScrollIndicatorProps) => {
-
-    const [scrollWidth, setScrollWidth] = useState(0);
-
-    const scroll = useScroll({
-        container: mainRef,
-    })
-
-    useMotionValueEvent(scroll.scrollYProgress, "change", (latest) => {
-        setScrollWidth(latest * 100)
-    })
+const ScrollIndicator = ({ progress }: ScrollIndicatorProps) => {
 
     return (
         <div className="absolute top-[50%] translate-y-[-50%] end-10 z-40 hidden lg:block">
             <div className="w-1.5 bg-white/10 rounded-full h-24 mb-4 overflow-hidden">
-                <div className={`bg-accent w-1.5 rounded-full`} style={{ height: Math.ceil(scrollWidth) + '%' }}></div>
+                <div className={`bg-accent w-1.5 rounded-full`} style={{ height: progress + '%' }}></div>
             </div>
         </div>
     )
